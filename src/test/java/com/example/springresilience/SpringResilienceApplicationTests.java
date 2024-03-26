e2698f0d682e5523ae40be317beddcd9b1539a7e
seungduru <dlatmddn468@gmail.com>
2024-03-26 10:45:30 +0900

    Initial commit

---
+++ src/test/java/com/example/springresilience/SpringResilienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springresilience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResilienceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
